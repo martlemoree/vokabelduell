01d066d31b5053e5f57563a201dc20040988d96a
Stellasalv <s0576792@htw-berlin.de>
2022-05-19 10:56:33 +0200

    changed parent pom

--- game_management.impl/src/main/java/de/htwberlin/kba/game_management/impl/RoundServiceImpl.java
+++ game_management.impl/src/main/java/de/htwberlin/kba/game_management/impl/RoundServiceImpl.java
@@ -5,6 +5,7 @@ import de.htwberlin.kba.game_management.export.Round;
 import de.htwberlin.kba.game_management.export.RoundService;
 import de.htwberlin.kba.user_management.export.User;
 import de.htwberlin.kba.vocab_management.export.VocabList;
+import java.util.Scanner;
 import sun.jvm.hotspot.tools.SysPropsDumper;
 
 import java.util.List;
@@ -15,16 +16,20 @@ public class RoundServiceImpl implements RoundService {
     public VocabList chooseVocablist(List<VocabList> randomVocabLists) {
         // hier kommt die user-interaktion mit der Konsole hin aber ich weiß echt nicht mehr wie das geht :)
 
-        String ausgabe = "Welche Vokabelliste möchtest du wählen? \n " +
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welche Vokabelliste möchtest du wählen? \n " +
                 "1. " + randomVocabLists.get(0).getName() + ",\n " +
                 "2. " + randomVocabLists.get(1).getName() + "oder \n " +
-                "3. " + randomVocabLists.get(2).getName() + "?";
+                "3. " + randomVocabLists.get(2).getName() + "?");
 
-        if (ausgabe.contains("1")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
+        String eingabeText = scanner.next();
+
+        if (eingabeText.contains("1")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
             return randomVocabLists.get(0);
-        } if (ausgabe.contains("2")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
+        } if (eingabeText.contains("2")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
             return randomVocabLists.get(1);
-        } if (ausgabe.contains("3")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
+        } if (eingabeText.contains("3")) { //hier wird der user input entgegengenommen, er kann "1", "2" oder "3" eingeben
             return randomVocabLists.get(2);
         } else {
             chooseVocablist(randomVocabLists); //geht das? (Gedanke: Einfach von vorne, da User-Eingabe so nicht funktioniert hat. Endlos loop wenn der user es nicht hinbekommt :)
