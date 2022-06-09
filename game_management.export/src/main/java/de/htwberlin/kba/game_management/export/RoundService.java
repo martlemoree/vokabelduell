4c1aefef4a62a6d64f841d73ea89008ad0c6863c
AntjeGe <s0576789@htw-berlin.de>
2022-06-09 11:19:14 +0200

    RoundService Test

--- game_management.export/src/main/java/de/htwberlin/kba/game_management/export/RoundService.java
+++ game_management.export/src/main/java/de/htwberlin/kba/game_management/export/RoundService.java
@@ -8,14 +8,6 @@ import java.util.List;
 public interface RoundService {
 
     /**
-     * create a new round
-     * @param roundId unique identifier of the round
-     * @param game the game in which the round is created
-     * @return a new round
-     */
-    Round createRound(Long roundId, Game game);
-
-    /**
      * new round is created and directly added to the list of rounds of the current game
      *
      * @param game current game for which new round is needed
--- game_management.impl/src/test/java/de/htwberlin/kba/game_management/impl/RoundServiceTest.java
+++ game_management.impl/src/test/java/de/htwberlin/kba/game_management/impl/RoundServiceTest.java
@@ -1,17 +1,51 @@
 package de.htwberlin.kba.game_management.impl;
 
 import de.htwberlin.kba.game_management.export.Game;
+import de.htwberlin.kba.game_management.export.QuestionService;
 import de.htwberlin.kba.game_management.export.Round;
 import de.htwberlin.kba.game_management.export.RoundService;
 import de.htwberlin.kba.user_management.export.User;
 import de.htwberlin.kba.vocab_management.export.VocabList;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class RoundServiceTest {
+
+    /*
+    @Spy // object is partially mocked. the real methods are being called
+    @InjectMocks
+    private RoundService service;
+
+    @Mock
+    RoundDao roundDao;
+
+    @Mock
+    Game mock_game;*/
+
+    RoundDao roundDao = new RoundDaoImpl();
+    RoundServiceImpl service = new RoundServiceImpl(roundDao);
+
+
+    @DisplayName("checks whether the method returns a round")
+    @Test
+    public void testStartNewRound() {
+        User requester = new User(1L, "MartinTheBrain", "lol123");
+        User receiver = new User(2L, "stellomello", "123lol");
+        Game game = new Game(3L, requester, receiver);
+
+        Assert.assertNotNull(service.startNewRound(game));
+        Assert.assertEquals(service.startNewRound(game).getGame(), game);
+
+    }
 
     // optional
 /*
