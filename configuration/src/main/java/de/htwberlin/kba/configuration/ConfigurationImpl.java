e8d5c86f827a8cdceb5d957a4453a20b3f8c16eb
AntjeGe <s0576789@htw-berlin.de>
2022-06-22 16:58:35 +0200

    put kempas code in a separate file

--- configuration/src/main/java/de/htwberlin/kba/configuration/ConfigurationImpl.java
+++ configuration/src/main/java/de/htwberlin/kba/configuration/ConfigurationImpl.java
@@ -1,11 +1,5 @@
 package de.htwberlin.kba.configuration;
 
-import javax.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import de.htwberlin.kba.vokabelduell_ui.export.VokabellduellUi;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class ConfigurationImpl {
 
-    // hier fehlt noch code zur konfiguration für PersistenceContext, die von Herr Kempa geliefert werden soll
     private static ConfigurableApplicationContext context = new AnnotationConfigApplicationContext("de.htwberlin");
 
     public static void main(String[] args) {
@@ -22,16 +15,6 @@ public class ConfigurationImpl {
         controller.run();
     }
 
-    @Bean
-    public LocalEntityManagerFactoryBean entityManagerFactory() {
-        LocalEntityManagerFactoryBean localEmfBean = new LocalEntityManagerFactoryBean();
-        localEmfBean.setPersistenceUnitName("jpaDemoPU");
-        return localEmfBean;
-    }
 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
-    }
 
 }
