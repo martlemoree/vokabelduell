089e5daf14a0d2c5e37e9caa770b6898d46b4e3e
Stellasalv <s0576792@htw-berlin.de>
2022-07-21 17:19:25 +0200

    RestTemplateResponseErrorHandler eingefügt

---
+++ configuration_client/src/main/java/de/htwberlin/kba/configuration/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,20 @@
+package de.htwberlin.kba.configuration;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+@Component
+public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+    @Override
+    public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
+        return false;
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+
+    }
+}
