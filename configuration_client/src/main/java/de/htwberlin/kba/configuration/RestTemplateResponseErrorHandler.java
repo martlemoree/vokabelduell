185296582e5ebfec185dd4320ea7dbc2e28b180c
Stellasalv <s0576792@htw-berlin.de>
2022-07-21 17:25:28 +0200

    RestTemplateResponseErrorHandler ergänzt

--- configuration_client/src/main/java/de/htwberlin/kba/configuration/RestTemplateResponseErrorHandler.java
+++ configuration_client/src/main/java/de/htwberlin/kba/configuration/RestTemplateResponseErrorHandler.java
@@ -1,20 +1,39 @@
 package de.htwberlin.kba.configuration;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
+import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
+
 @Component
 public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+
+//    https://www.baeldung.com/spring-rest-template-error-handling
+
     @Override
     public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
-        return false;
+        return (
+                clientHttpResponse.getStatusCode().series() == CLIENT_ERROR
+                        || clientHttpResponse.getStatusCode().series() == SERVER_ERROR);
     }
 
     @Override
     public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
-
+        if (clientHttpResponse.getStatusCode()
+                .series() == HttpStatus.Series.SERVER_ERROR) {
+            // handle SERVER_ERROR
+        } else if (clientHttpResponse.getStatusCode()
+                .series() == HttpStatus.Series.CLIENT_ERROR) {
+            // handle CLIENT_ERROR
+            if (clientHttpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+//                throw new ChangeSetPersister.NotFoundException();
+            }
+        }
     }
 }
